53168d260effbf6873a3ec2f81238d795af6ee4e
D.M.Anuradha Kumara Dissanayake <d.m.a.k.dissanayake.1997@gmail.com>
2022-09-24 12:22:46 +0530

    Updated

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ fruits = ['melon', 'orng', 'app', 'guava', 'bomb']
 
 # initialize pygame and create window
 
-WIDTH = 800
+WIDTH = 1000
 HEIGHT = 500
 FPS = 12
 
@@ -25,31 +25,32 @@ RED = (255,0,0)
 GREEN = (0,255,0) 
 BLUE = (0,0,255)
 
-background = pygame.image.load('back.jpg')                                  #game background
-font = pygame.font.Font(os.path.join(os.getcwd(), 'comic.ttf'), 42)
+background = pygame.image.load('./img/bg.jpg')                                  #game background
+font = pygame.font.Font(os.path.join(os.getcwd(), './fonts/PDBI.ttf'), 42)
 score_text = font.render('Score : ' + str(score), True, (255, 255, 255))    #score display
-lives_icon = pygame.image.load('images/white_lives.png')                    #images that shows remaining lives
+lives_icon = pygame.image.load('./img/w_h.png')                    #images that shows remaining lives
 
 # Generalized structure of the fruit Dictionary
 def generate_random_fruits(fruit):
-    fruit_path = "images/" + fruit + ".png"
+    fruit_path = "img/" + fruit + ".png"
     data[fruit] = {
         'img': pygame.image.load(fruit_path),
-        'x' : random.randint(100,500),          #where the fruit should be positioned on x-coordinate
+        'x' : random.randint(100,500),
         'y' : 800,
-        'speed_x': random.randint(-10,10),      #how fast the fruit should move in x direction. Controls the diagonal movement of fruits
-        'speed_y': random.randint(-80, -60),    #control the speed of fruits in y-directionn ( UP )
-        'throw': False,                         #determines if the generated coordinate of the fruits is outside the gameDisplay or not. If outside, then it will be discarded
-        't': 0,                                 #manages the
+        'speed_x': random.randint(-10,10),
+        'speed_y': random.randint(-80, -60),
+        'throw': False,
+        't': 0,
         'hit': False,
     }
 
-    if random.random() >= 0.75:     #Return the next random floating point number in the range [0.0, 1.0) to keep the fruits inside the gameDisplay
+    if random.random() >= 0.75:
         data[fruit]['throw'] = True
     else:
         data[fruit]['throw'] = False
 
 # Dictionary to hold the data the random fruit generation
+
 data = {}
 for fruit in fruits:
     generate_random_fruits(fruit)
@@ -58,6 +59,7 @@ def hide_cross_lives(x, y):
     gameDisplay.blit(pygame.image.load("./img/r_h.png"), (x, y))
 
 # Generic method to draw fonts on the screen
+
 font_name = pygame.font.match_font('comic.ttf')
 def draw_text(display, text, size, x, y):
     font = pygame.font.Font(font_name, size)
@@ -67,12 +69,13 @@ def draw_text(display, text, size, x, y):
     gameDisplay.blit(text_surface, text_rect)
 
 # draw players lives
+
 def draw_lives(display, x, y, lives, image) :
     for i in range(lives) :
         img = pygame.image.load(image)
-        img_rect = img.get_rect()       #gets the (x,y) coordinates of the cross icons (lives on the the top rightmost side)
-        img_rect.x = int(x + 35 * i)    #sets the next cross icon 35pixels awt from the previous one
-        img_rect.y = y                  #takes care of how many pixels the cross icon should be positioned from top of the screen
+        img_rect = img.get_rect()
+        img_rect.x = int(x + 35 * i)
+        img_rect.y = y
         display.blit(img, img_rect)
 
 # show game over display & front display
